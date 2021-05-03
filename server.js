b14d0db2ec32c63d3e1a92a49ac744d67b491fed
‘Timothy <Timothy.viccari@gmail.com>
2021-05-03 16:55:13 -0400

    add mongoose atlas

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 'use strict';
+
+//==========dependencies==========//
 require('dotenv').config();
 const express = require('express');
-const app = express();
 const cors = require('cors');
-
+const app = express();
+const ATLAS = process.env.ATLAS;
+const PORT = process.env.PORT || 3002;
 app.use(cors());
 app.use(express.json());
-app.get('/',(request, response) => {
-  response.send('server is live');
-});
 
-
+//==========mongoose set up==========/
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(`${ATLAS}`, {useNewUrlParser: true, useUnifiedTopology: true});
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
@@ -20,6 +20,10 @@ db.once('open', function() {
   console.log('Mongoose is connected');
 });
 
+//========routes========//
+app.get('/',(request, response) => {
+  response.send('server is live');
+});
+  
 
-const PORT = process.env.PORT || 3002;
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
